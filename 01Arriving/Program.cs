b84909d6802917318eb9b2f532a70966f7c63b6c
ignatovasashka <ignatovasashka@gmail.com>
2021-01-05 14:42:32 +0200

    Add project files.

---
+++ 01Arriving/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01Arriving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex =
+@"(?<name>[^!@#$?])(?<length>[0-9])(?<code>)";
+            string input = Console.ReadLine();
+            MatchCollection matched = Regex.Matches(input, regex);
+
+            foreach (Match date in matched)
+            {
+                var name = date.Groups["name"].Value;
+                var length = date.Groups["length"].Value;
+                var code = date.Groups["code"].Value;
+
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+
+
+
+
+
+
+
+
+
+            //string[] input = Console.ReadLine().Split("=");
+            //
+            //string peakName = input[0];
+            //string[] lengthAndGeohash = input[1].Split("<<");
+            //string length = lengthAndGeohash[0];
+            //string geohash = lengthAndGeohash[1];
+            //
+            //string regex = @"[^!@#$?]";
+            //
+            //MatchCollection matched =
+            //              Regex.Matches(peakName, regex);
+            //
+            //
+            //
+
+            //char[] oddChars = { '!', '@', '#', '$', '?' };
+            //if (peakName.Contains('!'))
+            //{
+            //    peakName.Remove(peakName.IndexOf('!'),1);
+            //}
+            //
+            //Console.WriteLine(peakName);
+        }
+    }
+}
---
+++ 02OnTheWay/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02OnTheWay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> storeAndItems = new Dictionary<string, List<string>>();
+
+            string[] input = Console.ReadLine().Split("->");
+            string command = input[0];
+
+            while (command != "END")
+            {
+                string storeName = input[1];
+                List<string> itemsInStore = new List<string>();
+
+                if (command == "Add")
+                {
+                    string[] tokensItems = input[2].Split(",");
+                    foreach (var item in tokensItems)
+                    {
+                        itemsInStore.Add(item);
+                    }
+
+                    if (!storeAndItems.ContainsKey(storeName))
+                    {
+                        storeAndItems.Add(storeName, itemsInStore);
+                    }
+                    else
+                    {
+
+                        foreach (var item in tokensItems)
+                        {
+                            storeAndItems[storeName].Add(item);
+
+                        }
+
+                    }
+                }
+                else if(storeAndItems.ContainsKey(storeName))
+                {
+                    storeAndItems.Remove(storeName);
+                }
+
+                input = Console.ReadLine().Split("->");
+                command = input[0];
+            }
+
+            Console.WriteLine("Stores list:");
+            foreach (var kvp in storeAndItems
+                .OrderByDescending(x=>x.Value.Count)
+                .ThenByDescending(x=>x.Key))
+            {
+                Console.WriteLine($"{kvp.Key}");
+                foreach (var item in storeAndItems[kvp.Key])
+                {
+                    Console.WriteLine($"<<{item}>>");
+                }
+            }
+        }
+    }
+}
+
